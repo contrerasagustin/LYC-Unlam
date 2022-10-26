34815c1b757205dee149bc1ea57140763f0849ac
unknown <facundo.gabriel.jimenez@gmail.com>
2022-10-26 14:11:07 -0300

    fix bug celda while

--- src/main/java/lyc/compiler/polacaInversa/PolacaInversaManager.java
+++ src/main/java/lyc/compiler/polacaInversa/PolacaInversaManager.java
@@ -28,16 +28,16 @@ public class PolacaInversaManager {
                 pila.add(lista.size());
                 break;
             case "JMP":
-                int auxInt = pila.peek()  - 5;
-                System.out.println("aux int"+ auxInt);
-                System.out.println("lista"+ lista.get(auxInt));
+                int auxInt = pila.peek()  - 4;
+                System.out.println("aux int "+ auxInt);
+                System.out.println("lista "+ lista.get(auxInt));
                 lista.add("JMP");
                 while (auxInt != 0){
                     if(! lista.get(auxInt-1).contains("#")){
                         lista.add("#"+(auxInt));
                         break;
                     }else{
-                        auxInt = auxInt  - 5;
+                        auxInt = auxInt  - 4;
                     }
                 }
                 if(auxInt==0)
